bf2930a47fda848798092627d51c1028acc0081d
Callum Lees <hi@c-l.dev>
2021-12-10 01:07:35 +0000

    Added solution comment on _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import "../styles/fonts.css";
 
 // TODO: 10 Dec 2021 - Install AOSJS, init here to use across all components.
+// TODO: Solution: https://github.com/michalsnik/aos/issues/574
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;

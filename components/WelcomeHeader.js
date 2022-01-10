b105200b1441e2178707e0e57d1693b088814ca7
Callum Lees <hi@c-l.dev>
2022-01-10 20:43:05 +0000

    Updated font size at different breakpoints and removed unnecessary classes

--- components/WelcomeHeader.js
+++ components/WelcomeHeader.js
@@ -3,18 +3,18 @@ import Image from "next/image";
 const WelcomeHeader = () => {
   return (
     <div className="w-full relative">
-      <div className="w-full h-full top-0 left-0 absolute"></div>
+      <div className="w-full h-full left-0 absolute"></div>
       <Image
           src="/media/mathieu-ramus-gSd173Bwsqk-unsplash.jpg"
           className="object-cover w-screen z-0 cover mx-auto"
           width="2600"
           height="1200"
-          alt="Polar bear playing"
+          alt="Polar bear playing on the mountain floor."
           data-aos="fade-up"
           data-aos-easing="linear"
           data-aos-duration="1000"
         />
-      <div className="w-full h-full flex flex-col-reverse absolute top-0 left-0 justify-center mx-auto md:pl-28 items-center md:items-start">
+      <div className="w-full h-full flex flex-col-reverse absolute top-0 justify-center mx-auto md:pl-28 items-center md:items-start">
       <div className="flex">
         <div className="space-x-4">
           <a href="/#">
@@ -29,7 +29,7 @@ const WelcomeHeader = () => {
           </a>
         </div>
       </div>
-      <div className="text-4xl md:text-9xl font-bold text-white">Time For Action</div>
+      <div className="text-4xl sm:text-5xl md:text-8xl font-bold text-white">Time For Action</div>
       <div className="text-sm md:text-xl text-white mb-2">Donate once. Help twice.</div>
     </div>
   </div>

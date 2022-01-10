57aceabd95a161e6abf1b29cd3a82be049478332
Callum Lees <hi@c-l.dev>
2022-01-10 14:38:50 +0000

    Updated WelcomeHeader component

--- components/WelcomeHeader.js
+++ components/WelcomeHeader.js
@@ -2,55 +2,37 @@ import Image from "next/image";
 
 const WelcomeHeader = () => {
   return (
-    <div data-aos="fade-up" className="flex mx-auto max-w-6xl md:mt-24">
-      <ul className="grid grid-cols-1 gap-8 md:grid-cols-2 px-10 md:gap-8">
-        {/* Practical */}
-        <li className="md:text-center items-center rounded-lg md:h-full h-full px-3">
-          <Image
-            src="/media/eva-blue-wmdVFw9ubKs-unsplash.jpg"
-            alt="Man"
-            className="object-cover"
-            width="2400"
-            height="1634"
-          />
-        </li>
-        {/* Reliable */}
-        <li className="justify-center align-items mt-1 items-center h-full">
-          <p className="pt-9 text-left uppercase tracking-widest text-sm font-light">
-            CHANGE IS COMING
-          </p>
-          <h1 className="text-green-500 text-6xl  text-left font-bold">
-            Globeco
-          </h1>
-          <a href="/#" className="font-semibold">
-            We are going to change the{" "}
-            <a href="/#" className="hover:underline text-green-500">
-              world
-            </a>{" "}
-            .
+    <div className="w-full relative">
+      <div className="w-full h-full top-0 left-0 absolute"></div>
+      <Image
+          src="/media/mathieu-ramus-gSd173Bwsqk-unsplash.jpg"
+          className="object-cover w-screen z-0 cover mx-auto"
+          width="2600"
+          height="1200"
+          alt="Polar bear playing"
+          data-aos="fade-up"
+          data-aos-easing="linear"
+          data-aos-duration="1000"
+        />
+      <div className="w-full h-full flex flex-col-reverse absolute top-0 left-0 justify-center mx-auto md:pl-28 items-center md:items-start">
+      <div className="flex">
+        <div className="space-x-4">
+          <a href="/#">
+            <button className="text-xs md:text-md py-2 underline hover:text-gray-200 px-3 rounded-md font-bold  tracking-widest text-white uppercase mb-0 transition duration-300">
+              Donate
+            </button>
           </a>
-          <p className="text-sm mt-2 font-light mb-4 md:mb-0">
-            We all know the climate clock is ticking, but there is still time
-            left for action. <br />
-            <br />
-          </p>
-          <div className="flex">
-            <div className="space-x-4">
-              <a href="/#">
-                <button className="text-xs md:text-md py-2 border-2 border-green-500 px-3 rounded-md font-bold bg-green-500 tracking-widest text-white uppercase mb-0 hover:text-green-500 hover:bg-white transition duration-300">
-                  Donate
-                </button>
-              </a>
-              <a href="/#">
-                <button className="text-xs md:text-md py-2 px-3 rounded-md font-bold border-2 border-green-500 tracking-widest text-green-500 uppercase mb-0 hover:bg-green-500 hover:text-white transition duration-300">
-                  About us
-                </button>
-              </a>
-            </div>
-          </div>
-        </li>
-      </ul>
+          <a href="/#">
+            <button className="text-xs md:text-md py-2 underline hover:text-gray-200 px-3 rounded-md font-bold  tracking-widest text-white uppercase mb-0 transition duration-300">
+              About us
+            </button>
+          </a>
+        </div>
+      </div>
+      <div className="text-4xl md:text-9xl font-bold text-white">Time For Action</div>
+      <div className="text-sm md:text-xl text-white mb-2">Donate once. Help twice.</div>
     </div>
+  </div>
   );
 };
 

1b77086f21b223290c22a9fce8e376d1a173c11e
Callum Lees <hi@c-l.dev>
2022-01-10 22:48:52 +0000

    Removed shadow and read more button

--- components/CardGallery/CardOne.js
+++ components/CardGallery/CardOne.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CardOne() {
     return (
-        <div className='rounded-2xl p-6 shadow-xl'>
+        <div className='p-6'>
             <li className="mx-auto items-center justify-center text-left p-2 md:h-full h-full">
                 <h1 className='text-7xl font-bold tracking-tight'>40</h1>
                 <h5 className="text-lg font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500">
@@ -11,11 +11,6 @@ function CardOne() {
                 <p className="text-sm mx-auto mt-2">
                 From the Amazon to New Guinea to the Congo Basin, communities all over the world are fighting deforestation. 
                 </p>
-                <button className='mt-4'>
-                    <a href='#' className='transition duration-300 uppercase hover:text-black text-xs font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500'>
-                        Read More &rarr;
-                    </a>
-                </button>
             </li>
         </div>
     )
--- components/CardGallery/CardThree.js
+++ components/CardGallery/CardThree.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CardThree() {
     return (
-        <div className='rounded-2xl p-6 shadow-xl'>
+        <div className='p-6'>
             <li className="mx-auto items-center justify-center text-left p-2 md:h-full h-full">
                 <h1 className='text-7xl font-bold tracking-tight'>48M</h1>
                 <h5 className="text-lg font-bold mt-2 text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500">
@@ -11,11 +11,6 @@ function CardThree() {
                 <p className="text-sm mx-auto mt-2">
                     Carbon is being stored and kept out of the atmosphere in all of our partnerships in the Amazon, Congo and New Guinea rainforests.
                 </p>
-                <button className='mt-4'>
-                <a href='#' className='transition duration-300 uppercase hover:text-black text-xs font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-pink-500'>
-                        Read More &rarr;
-                    </a>
-                </button>
             </li>
         </div>
     )

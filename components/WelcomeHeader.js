ab5a2a0bfd368d2664f3e3fcebd0436a4ba8be16
Callum Lees <hi@c-l.dev>
2022-01-11 13:09:49 +0000

    Added passHref for build

--- components/WelcomeHeader.js
+++ components/WelcomeHeader.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Link from 'next/link'
 
 const WelcomeHeader = () => {
   return (
@@ -10,20 +10,20 @@ const WelcomeHeader = () => {
       <div className="w-full h-full flex flex-col-reverse absolute top-0 justify-center mx-auto items-center">
       <div className="flex">
         <div className="space-x-4">
-          <a href="/#" data-aos="fade-up"
+          <Link href="/#" data-aos="fade-up"
             data-aos-easing="linear"
-            data-aos-duration="2000">
+            data-aos-duration="2000" passHref>
             <button className="text-xs md:text-md py-2 underline hover:text-gray-200 px-3 rounded-md font-bold  tracking-widest  uppercase mb-0 transition duration-300">
               Donate
             </button>
-          </a>
-          <a href="/#" data-aos="fade-up"
+          </Link>
+          <Link href="/#" data-aos="fade-up"
             data-aos-easing="linear"
-            data-aos-duration="2000">
+            data-aos-duration="2000" passHref>
             <button className="text-xs md:text-md py-2 underline hover:text-gray-200 px-3 rounded-md font-bold  tracking-widest  uppercase mb-0 transition duration-300">
               About us
             </button>
-          </a>
+          </Link>
         </div>
       </div>
       <div className="text-4xl sm:text-5xl md:text-8xl font-bold" data-aos="fade-up"
